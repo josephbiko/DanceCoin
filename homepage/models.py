4d280c7cd492515a615fece53944ec41a88f49a8
joseph <josephgk@hotmail.nl>
2017-10-21 00:34:17 +0200

    Qr code working

--- homepage/models.py
+++ homepage/models.py
@@ -7,6 +7,8 @@ from django.contrib.auth.models import User
 class account(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     tokens = models.FloatField(default=0)
+    hash = models.CharField(max_length=64,default="81DBBQ837AERB0MYVTN5DG1BBL3B8XH5ILO7P84YPQQS81PFLGUN0QWHQHY1HJZ0")
+
 
 
 
--- homepage/views.py
+++ homepage/views.py
@@ -5,10 +5,16 @@ from django.shortcuts import render
 from django.views.generic import View,TemplateView
 from models import account
 # Create your views here.
+
+
 class home(TemplateView):
     template_name = "homepage/login.html"
 
     def get(self, request, *args, **kwargs):
-        context = {"login": request.user.is_authenticated(), "user":account.objects.get(user=request.user)}
+        user = None
+        if request.user.is_authenticated():
+            user = account.objects.get(user=request.user)
+
+        context = {"login": request.user.is_authenticated(), "user":user}
 
         return render(request,"homepage/home.html",context=context)
